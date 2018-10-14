818760403c3f1b04f5b2b8cebda0a9592caf762d
aayushbansal <aayushb@cs.cmu.edu>
2018-10-14 00:32:12 -0400

    updated

--- scripts/run_reCycle_gan.sh
+++ scripts/run_reCycle_gan.sh
@@ -1,2 +1,2 @@
 #!./scripts/train_recycle.sh
-python train.py --dataroot ./datasets/<DATASET-NAME> --name <NAME> --model reCycle_gan  --which_model_netG resnet_6blocks --which_model_netP unet_128 --npf 8  --dataset_mode unaligned_triplet  --no_dropout --gpu 3 --pool_size 0 
+python train.py --dataroot ./datasets/<DATASET-NAME> --name <NAME> --model reCycle_gan  --which_model_netG resnet_6blocks --which_model_netP unet_256 --npf 8  --dataset_mode unaligned_triplet  --no_dropout --gpu 3 --pool_size 0 
